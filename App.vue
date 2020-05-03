e53a79dc1d1e7c586cd4e5a62454db9cc9ac6f31
twofloor <1206284818@qq.com>
2020-05-04 00:47:49 +0800

    初始化工程

---
+++ App.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import 'assets/css/uni.css';
+	@import 'assets/css/iconfont.css';
+</style>

52dacdcfb23fefc63a16134b11e5cffabb0d6de8
twofloor <1206284818@qq.com>
2020-05-05 16:16:34 +0800

    登录

---
+++ pages/login/login.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<view class="login-logo">
+			<image mode="aspectFit" src="../../assets/img/Octocat.jpg" />
+		</view>
+		<u-cell-group>
+			<u-field v-model="mobile" label="手机号" placeholder="请填写手机号">
+			</u-field>
+			<u-field v-model="code" label="验证码" placeholder="请填写验证码">
+			</u-field>
+		</u-cell-group>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="less">
+	.login-logo {
+		display: table-cell;
+		vertical-align: middle;
+		text-align: center;
+		width: calc(100vw);
+		height: 30vh;
+
+		uni-image {
+			width: 200rpx;
+			height: 200rpx;
+		}
+	}
+</style>

674ed2935de107c1566c0a3c0471d5034bbdb441
liupan <1206284818@qq.com>
2020-05-10 22:52:26 +0800

    api

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import App from './App'
+import MinRequest from './utils//MinRequest'
+import minRequest from './api/api'
+import MinCache from './utils/MinCache'
 import uView from "@/uview"
 Vue.use(uView)
+Vue.use(MinRequest)
+Vue.use(MinCache)
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
+	minRequest,
 	...App
 })
 app.$mount()

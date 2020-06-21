a6c4b87deb7e05db3366555cfaa3b8cb7b29bee6
twofloor <1206284818@qq.com>
2020-06-21 15:27:22 +0800

    个人信息

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -112,41 +112,7 @@
 				}
 			},
 			async getAuthUser() {
-				// const user = await this.$minApi.getAuthUser()
-				// this.authUser = user
-				this.authUser = {
-					"login": "silianpan",
-					"id": 7081687,
-					"node_id": "MDQ6VXNlcjcwODE2ODc=",
-					"avatar_url": "https://avatars1.githubusercontent.com/u/7081687?v=4",
-					"gravatar_id": "",
-					"url": "https://api.github.com/users/silianpan",
-					"html_url": "https://github.com/silianpan",
-					"followers_url": "https://api.github.com/users/silianpan/followers",
-					"following_url": "https://api.github.com/users/silianpan/following{/other_user}",
-					"gists_url": "https://api.github.com/users/silianpan/gists{/gist_id}",
-					"starred_url": "https://api.github.com/users/silianpan/starred{/owner}{/repo}",
-					"subscriptions_url": "https://api.github.com/users/silianpan/subscriptions",
-					"organizations_url": "https://api.github.com/users/silianpan/orgs",
-					"repos_url": "https://api.github.com/users/silianpan/repos",
-					"events_url": "https://api.github.com/users/silianpan/events{/privacy}",
-					"received_events_url": "https://api.github.com/users/silianpan/received_events",
-					"type": "User",
-					"site_admin": false,
-					"name": "silianpan",
-					"company": "Seal Studio",
-					"blog": "http://silianpan.cn",
-					"location": "Chengdu, Sichuan",
-					"email": "liu.pan@silianpan.cn",
-					"hireable": null,
-					"bio": "Feel no regret for this life.",
-					"public_repos": 28,
-					"public_gists": 0,
-					"followers": 14,
-					"following": 23,
-					"created_at": "2014-03-27T13:33:01Z",
-					"updated_at": "2020-05-23T09:34:39Z"
-				}
+				this.authUser = await this.$minApi.getAuthUser()
 				this.numInfo = {
 					public_repos: this.authUser.public_repos,
 					followers: this.authUser.followers,

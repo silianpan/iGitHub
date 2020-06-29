4aa51e42d4e08d069b846214e43132ead1687646
liupan <1206284818@qq.com>
2020-06-29 22:47:12 +0800

    news

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -3,15 +3,11 @@
 		<view class="order" v-for="(item, index) in receivedEvents" :key="index">
 			<view class="item">
 				<view class="left">
-					<image class="avatar" :src="item.avatar" mode="aspectFill"></image>
+					<image class="avatar" :src="item.actor.avatar_url" mode="aspectFill"></image>
 				</view>
 				<view class="content">
-					<view style="flex-flow: row wrap; justify-content: space-between; align-items: center; display: flex;">
-						<view class="title u-line-2">{{ item.name }}<text class="sub-title">{{' (' + item.username + ')' }}</text></view>
-						<uni-badge class="uni-badge-left-margin" :text="index+1" type="primary" size="small" />
-					</view>
+					<view class="title u-line-2">{{ item.actor.login }}<text>{{ ' ' + item.payload.action + ' ' }}</text><text class="sub-title">{{ item.repo.name }}</text></view>
 					<view class="remark2"><text class="iconfont iconbook1 margin-right-xs" />{{ item.repo.name }}</view>
-					<view class="description">{{ item.repo.description }}</view>
 				</view>
 			</view>
 		</view>
@@ -19,18 +15,22 @@
 </template>
 
 <script>
+	import { mapGetters } from 'vuex'
 	export default {
 		data() {
 			return {
 				receivedEvents: []
 			}
 		},
+		computed: {
+			...mapGetters(['authUserInfo'])
+		},
 		onShow() {
 			this.listAuthUserReceivedEvents()
 		},
 		methods: {
 			async listAuthUserReceivedEvents() {
-				this.receivedEvents = await this.$minApi.listAuthUserReceivedEvents()
+				this.receivedEvents = await this.$minApi.listAuthUserReceivedEvents(this.authUserInfo.name)
 			}
 		}
 	}

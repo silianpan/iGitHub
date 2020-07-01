68e1ba91dadcf90c3492cbae25dd7f08a7fa0a69
liupan <1206284818@qq.com>
2020-07-01 14:54:25 +0800

    下拉加载

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -1,5 +1,6 @@
 <template>
-	<view>
+	<scroll-view scroll-y style="height: 100%;width: 100%;" @scrolltolower="reachBottom" refresher-enabled
+	 :refresher-triggered="triggered" @refresherrefresh="onRefresh" @refresherrestore="onRestore" @refresherpulling="onPulling">
 		<view class="order" v-for="(item, index) in receivedEvents" :key="index">
 			<view class="item">
 				<view class="left">
@@ -10,11 +11,13 @@
 				</view>
 			</view>
 		</view>
-	</view>
+	</scroll-view>
 </template>
 
 <script>
-	import { mapGetters } from 'vuex'
+	import {
+		mapGetters
+	} from 'vuex'
 	import Event from './event'
 	export default {
 		components: {
@@ -22,25 +25,43 @@
 		},
 		data() {
 			return {
+				_freshing: false,
+				triggered: false,
 				receivedEvents: []
 			}
 		},
 		computed: {
 			...mapGetters(['authUserInfo'])
 		},
-		onShow() {
-			this.listAuthUserReceivedEvents()
+		onLoad() {
+			this._freshing = false
+			// 触发onRefresh来加载自己的数据，如果不用这种方式，不要在此改变triggered的值
+			this.listAuthUserReceivedEvents(true, false)
 		},
 		methods: {
-			async listAuthUserReceivedEvents() {
+			async listAuthUserReceivedEvents(triggered, freshing) {
 				this.receivedEvents = await this.$minApi.listAuthUserReceivedEvents(this.authUserInfo.name)
+				this.triggered = triggered
+				this._freshing = freshing
 			},
 			clickRepoDetail(repoName) {
 				const tmp = _.split(repoName, '/')
 				uni.navigateTo({
 					url: `/pages/repos/detail?owner=${tmp[0]}&repo=${tmp[1]}`
 				})
-			}
+			},
+			onPulling(e) {},
+			onRefresh() {
+				if (this._freshing) return
+				this._freshing = true
+				if (!this.triggered) // 界面下拉触发，triggered可能不是true，要设为true  
+					this.triggered = true
+				this.listAuthUserReceivedEvents(false, false)
+			},
+			onRestore() {},
+			onAbort() {},
+			// scroll-view到底部加载更多
+			reachBottom() {}
 		}
 	}
 </script>

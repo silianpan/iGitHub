41194a21cfb12cfb4ee669f896487b1c81b97614
liupan <1206284818@qq.com>
2020-07-30 16:10:51 +0800

    完成i18n

--- i18n/index.js
+++ i18n/index.js
@@ -4,8 +4,15 @@ import enUS from './en-US'
 import zhCN from './zh-CN'
 Vue.use(VueI18n)
 
+let lang = uni.getStorageSync('_lang')
+if (!lang) {
+	const res = uni.getSystemInfoSync()
+	uni.setStorageSync('_lang', res.language)
+	lang = res.language
+}
+
 const i18n = new VueI18n({
-	locale: 'en',
+	locale: lang,
 	messages: {
 		'en': enUS,
 		'zh-CN': zhCN
